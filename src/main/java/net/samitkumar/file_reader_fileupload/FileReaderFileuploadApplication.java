832e51c28479a0160c9c404541312d3a6250f81f
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2024-06-04 14:54:51 +0200

    Indial commit

---
+++ src/main/java/net/samitkumar/file_reader_fileupload/FileReaderFileuploadApplication.java
@@ -0,0 +1,13 @@
+package net.samitkumar.file_reader_fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileReaderFileuploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileReaderFileuploadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/samitkumar/file_reader_fileupload/FileReaderFileuploadApplicationTests.java
@@ -0,0 +1,13 @@
+package net.samitkumar.file_reader_fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileReaderFileuploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
